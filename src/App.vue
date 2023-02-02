c6b98162b42b65450839a3a705509d4483da3b99
LucaDalRi <euro2xd@gmail.com>
2023-02-02 16:45:48 +0100

    AppHeader Completata

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+
+// export default {
+//   name: "App",
+//   components: {
+//     HelloWorld,
+//   }
+// };
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss" scoped>
+#app{
+  height: 100vh;
+  width: 100vh;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,64 @@
+<script>
+
+export default {
+  name: 'AppHeader',
+}
+
+</script>
+
+<template>
+  <div class="containerHeader">
+    <div class="containerLogoHeader">
+      <img src="../assets/img/dc-logo.png" alt="DC Logo Header">
+    </div>
+    <div class="containerLinksHeader">
+      <ul>
+        <li><a href="#">Character</a></li>
+        <li><a class="active" href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.containerHeader{
+  display: flex;
+  align-items: center;
+  height: 150px;
+  text-align: center;
+  border: 2px dashed red;
+};
+.containerLogoHeader{
+  width: 40%;
+};
+  
+.containerLinksHeader{
+  width: 60%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & ul{
+    display: flex;
+    justify-content: center;
+    list-style: none;
+    & li a{
+      text-decoration: none;
+      color: rgb(0, 0, 0);
+      margin: 0 20px;
+      &.active{
+        color: rgb(29, 79, 218);
+      };
+    };
+  };
+};
+
+</style>
