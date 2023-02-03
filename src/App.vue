f38868092854a5e191280908cbb52f967326d728
LucaDalRi <euro2xd@gmail.com>
2023-02-03 09:37:06 +0100

    Corretto setup nel tag script in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
-<script setup>
+<script>
 import AppHeader from './components/AppHeader.vue';
 import AppMainSection1 from './components/AppMainSection1.vue';
 import AppMainSection2 from './components/AppMainSection2.vue'
 
-// export default {
-//   name: "App",
-//   components: {
-//     HelloWorld,
-//   }
-// };
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMainSection1,
+    AppMainSection2
+  }
+};
 </script>
 
 <template>
